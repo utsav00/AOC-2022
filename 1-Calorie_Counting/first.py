e17c1feeb4f4701accd181b46f88030f430387a0
Utsav Parmar <uparmar@springhealth.com>
2022-12-09 12:20:39 +0530

    Add: day 1 challenge

---
+++ 1-Calorie_Counting/first.py
@@ -0,0 +1,20 @@
+def process_input():
+    file_path = './1-Calorie_Counting/calories.txt' 
+    with open(file_path, 'r') as f:
+        content = f.read().split('\n\n')
+        return [sum(list(map(int, each_total.split('\n')))) for each_total in content]
+
+def first_part(calories):
+    return max(calories)
+
+def second_part(calories):
+    return sum(sorted(calories, reverse=True)[0:3])
+
+if __name__ == "__main__":
+    calories = process_input()
+    
+    print("--Part 1--")
+    print(first_part(calories))
+    
+    print("--Part 2--")
+    print(second_part(calories))
